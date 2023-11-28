3fa3262e5f50f060eb42964e9eea252a0dad26d6
mohamed ait abdenni <seulmoi00@gmail.com>
2023-11-29 00:07:40 +0100

    Code changes

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,6 @@ function App() {
         Click to learn more by reading the docs.
       </p>
 
-      <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
 
     </>
   )
